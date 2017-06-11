4064bd26d6afc8a64ee19dcf6476ade12c298ab1
Miki <myoshimu@users.noreply.github.com>
2017-06-11 18:12:00 +0900

    Create statespace.R

--- statespace.R
+++ statespace.R
@@ -26,6 +26,7 @@ exp(outMLE1$par)
 sqrt(diag(avar))
 mod1 <- buildModel(outMLE1$par)
 mod.filt1<-dlmFilter(y,mod1)
+
 #固定区間平滑化
 mod.smooth1 <- dlmSmooth(mod.filt1)
 a1<-dropFirst(mod.smooth1$s[,1])
@@ -33,6 +34,7 @@ a2<-dropFirst(mod.smooth1$s[,2])
 a3<-dropFirst(mod.smooth1$s[,3])
 a4<-dropFirst(mod.smooth1$s[,4])
 a5<-dropFirst(mod.smooth1$s[,5])
+
 #平滑化のグラフ
 par(mfrow=c(3,2)) 
 plot(a1,type="l",xlab="days",ylab="beta_0" ,ylim=c(-0.5,0.3))
@@ -40,6 +42,7 @@ plot(a2,type="l",xlab="days",ylab="beta_1",ylim=c(-12,-3))
 plot(a3,type="l",xlab="days",ylab="beta_2",ylim=c(0.8,2))
 plot(a4,type="l",xlab="days",ylab="beta_3",ylim=c(0.3,1.2))
 plot(a5,type="l",xlab="days",ylab="beta_4",ylim=c(-0.11,0))
+
 #自身の説明変数だけ（モデル2）
 y<-Dataset$LogPI_A
 x<-cbind(Dataset$LogPriceIndex_A,Dataset$Display_A)
@@ -55,6 +58,7 @@ outMLE2$par
 outMLE2$value
 exp(outMLE2$par)
 sqrt(diag(avar))
+
 #フルモデル−競合のエンド（モデル3）
 y<-Dataset$LogPI_A
 x<-cbind(Dataset$LogPriceIndex_A,Dataset$LogPriceIndex_B,Dataset$Display_A)
@@ -72,18 +76,21 @@ exp(outMLE3$par)
 sqrt(diag(avar))
 mod3 <- buildModel(outMLE3$par)
 mod.filt3<-dlmFilter(y,mod3)
+
 #固定区間平滑化
 mod.smooth3 <- dlmSmooth(mod.filt3)
 a1<-dropFirst(mod.smooth3$s[,1])
 a2<-dropFirst(mod.smooth3$s[,2])
 a3<-dropFirst(mod.smooth3$s[,3])
 a4<-dropFirst(mod.smooth3$s[,4])
+
 #平滑化のグラフ
 par(mfrow=c(2,2)) 
 plot(a1,type="l",xlab="days",ylab="beta_0" ,ylim=c(-0.5,0.3))
 plot(a2,type="l",xlab="days",ylab="beta_1",ylim=c(-12,-3))
 plot(a3,type="l",xlab="days",ylab="beta_2",ylim=c(0.8,2))
 plot(a4,type="l",xlab="days",ylab="beta_3",ylim=c(0.3,1.2))
+
 #フルモデル−競合価格（モデル4）
 y<-Dataset$LogPI_A
 x<-cbind(Dataset$LogPriceIndex_A,Dataset$Display_A,Dataset$Display_B)
